48d665fe907291dfd43affce774541ab47318b8a
Neha Gupta <neha.gupta3@ibm.com>
2021-03-31 17:27:37 +0530

    generate bom

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cyclonedx-go -o bom.xml
+
 cat > payload.json <<__HERE__
 {
   "projectName":"test1",
@@ -8,7 +10,7 @@ cat > payload.json <<__HERE__
 }
 __HERE__
 
-curl -X "PUT" http://341434f54337.ngrok.io/api/v1/bom \
+curl -X "PUT" http://19f8754685e8.ngrok.io/api/v1/bom \
      -H 'Content-Type: application/json' \
      -H 'X-API-Key: smbJt0FTNzDTxB8jKXWeJODniwZAHE6w' \
      -d @payload.json
