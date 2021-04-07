fff7e0c9ed0500e3aadcf5ea7142340ba069b281
Neha Gupta <neha.gupta3@ibm.com>
2021-04-07 15:04:27 +0530

    voilation file

--- getViolations.sh
+++ getViolations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-result=$(curl -X GET --header "Content-Type: application/json" -H "X-API-Key: smbJt0FTNzDTxB8jKXWeJODniwZAHE6w" "http://19f8754685e8.ngrok.io/api/v1/finding/project/cecbf7b4-076c-41e1-b4ae-69ea2e509271")
+result=$(curl -X GET --header "Content-Type: application/json" -H "X-API-Key: smbJt0FTNzDTxB8jKXWeJODniwZAHE6w" "http://43396afb705c.ngrok.io/api/v1/finding/project/cecbf7b4-076c-41e1-b4ae-69ea2e509271")
 echo "Response from server"
 echo $result  > vulnerabilities.json
 
-result=$(curl -X GET --header "Content-Type: application/json" -H "X-API-Key: smbJt0FTNzDTxB8jKXWeJODniwZAHE6w" "http://19f8754685e8.ngrok.io/api/v1/violation/project/cecbf7b4-076c-41e1-b4ae-69ea2e509271")
+result=$(curl -X GET --header "Content-Type: application/json" -H "X-API-Key: smbJt0FTNzDTxB8jKXWeJODniwZAHE6w" "http://43396afb705c.ngrok.io/api/v1/violation/project/cecbf7b4-076c-41e1-b4ae-69ea2e509271")
 echo "Response from server"
 echo $result  > violation.json
 
